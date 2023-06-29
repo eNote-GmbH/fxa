45be1a3ddb3f5e26736bfbe2637418a98e11298e
Bianca Danforth <biancadanforth@gmail.com>
2023-06-29 17:34:21 -0400

    feat(db): create carts table to store subscription checkout state
    Because:
    
    * We want a user to be able to resume where they left off in the checkout process.
    
    This commit:
    
    * Creates a carts table to store subscription checkout state per [SubPlat 3.0 Cart DB Structure](https://docs.google.com/document/d/1hUx-lbymI8It_Cn5AeUkVmwdc2_pqCJDqs8G9X6ia8c/edit?usp=sharing).
    
    Closes #FXA-7506

---
+++ packages/db-migrations/databases/fxa/patches/patch-138-139.sql
@@ -0,0 +1,27 @@
+-- -- Create cart table to store checkout Cart state when creating a subscription.
+-- -- An FxA user can have more than one Cart record e.g. if they have more than
+-- -- one subscription where each subscription was created in a different session.
+-- -- A Cart record may not be associated with an FxA user e.g. if they started
+-- -- the new user checkout flow but did not get to the point where we create an
+-- -- account.
+
+SET NAMES utf8mb4 COLLATE utf8mb4_bin;
+
+CALL assertPatchLevel('138');
+
+CREATE TABLE IF NOT EXISTS carts (
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    uid BINARY(16),
+    state ENUM('NEW', 'UPGRADE', 'PROCESSING', 'SUCCESS', 'FAIL') NOT NULL,
+    errorReasonId VARCHAR(255),
+    offeringConfigId VARCHAR(255) NOT NULL,
+    interval VARCHAR(255) NOT NULL,
+    experiment VARCHAR(255),
+    taxAddress JSONB,
+    createdAt BIGINT UNSIGNED NOT NULL,
+    updatedAt BIGINT UNSIGNED,
+    couponCode VARCHAR(255),
+    FOREIGN KEY (uid) REFERENCES accounts(uid) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+UPDATE dbMetadata SET value = '139' WHERE name = 'schema-patch-level';

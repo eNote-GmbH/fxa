fa3f14b8f6fbbded3b380b9e846890d3dd0b018f
Reino Muhl <reino.muhl@gmail.com>
2023-10-09 10:32:47 -0400

    fix(payments): config file missing in build Because:
    * payments-next heartbeat api returns 500 due to missing config file.
    
    This commit:
    
    * Reorders nest-typed-config loaders so that fileLoader is first, which
      results in an empty .env.json file to be included in the
      payments-next build.
    * Adds normalization step to config loader, for non-string values.
    
    Closes #

--- apps/payments/next/app/_nestapp/app.module.ts
+++ apps/payments/next/app/_nestapp/app.module.ts
@@ -15,9 +15,20 @@ import { RootConfig } from './config';
     TypedConfigModule.forRoot({
       schema: RootConfig,
       load: [
-        dotenvLoader({ separator: '__', ignoreEnvFile: true }),
-        fileLoader(),
+        fileLoader(), // fileLoader() needs to be first, else next build doesn't include .env.json in build
+        dotenvLoader({
+          separator: '__',
+          ignoreEnvFile: true,
+        }),
       ],
+      normalize(config) {
+        config.mysqlConfig.port = parseInt(config.mysqlConfig.port, 10);
+        config.mysqlConfig.connectionLimitMax = parseInt(
+          config.mysqlConfig.connectionLimitMax,
+          10
+        );
+        return config;
+      },
     }),
   ],
   controllers: [],

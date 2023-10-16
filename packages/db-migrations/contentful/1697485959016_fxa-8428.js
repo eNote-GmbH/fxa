af24fb5971a428fbc45016bd97cc118ed7caa31e
Lisa Chan <lchan@mozilla.com>
2023-10-16 15:56:57 -0400

    chore(contentful): Update Contentful Content Model with newsletter fields

---
+++ packages/db-migrations/contentful/1697485959016_fxa-8428.js
@@ -0,0 +1,51 @@
+function migrationFunction(migration, context) {
+  const commonContent = migration.editContentType('commonContent');
+  const commonContentNewsletterLabelTextCode = commonContent.createField(
+    'newsletterLabelTextCode'
+  );
+  commonContentNewsletterLabelTextCode
+    .name('Newsletter Label Text Code')
+    .type('Symbol')
+    .localized(false)
+    .required(false)
+    .validations([
+      {
+        in: [
+          'new-user-subscribe-product-updates-mozilla',
+          'hubs',
+          'mdnplus',
+          'snp',
+        ],
+      },
+    ])
+    .defaultValue({ en: 'new-user-subscribe-product-updates-mozilla' })
+    .disabled(false)
+    .omitted(false);
+
+  const commonContentNewsletterSlug =
+    commonContent.createField('newsletterSlug');
+  commonContentNewsletterSlug
+    .name('Newsletter Slug')
+    .type('Array')
+    .localized(false)
+    .required(false)
+    .validations([])
+    .defaultValue({ en: ['mozilla-accounts'] })
+    .disabled(false)
+    .omitted(false)
+    .items({
+      type: 'Symbol',
+      validations: [
+        {
+          in: ['mozilla-accounts', 'hubs', 'mdnplus', 'security-privacy-news'],
+        },
+      ],
+    });
+  commonContent.changeFieldControl(
+    'newsletterLabelTextCode',
+    'builtin',
+    'radio'
+  );
+  commonContent.changeFieldControl('newsletterSlug', 'builtin', 'checkbox');
+}
+module.exports = migrationFunction;

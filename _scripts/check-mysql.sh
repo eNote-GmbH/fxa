b8c2e6a001ff4803c89a7e37e7de8d09024d8207
dschom <dschomburg@mozilla.com>
2023-10-17 17:11:55 -0700

    task(many): Fix startup isssue with new docker 4.24.2

--- _scripts/check-mysql.sh
+++ _scripts/check-mysql.sh
@@ -4,13 +4,16 @@ PORT=${1:-3306}
 HOST=${2:-localhost}
 RETRY=60
 for i in $(eval echo "{1..$RETRY}"); do
-  if echo PING | nc "$HOST" "$PORT" | grep -q 'mysql'; then
+  if nc -vz localhost 3306 2>&1 | grep -e 'mysql.*succeeded!'; then
+    echo "SUCCESS"
     exit 0
   else
+    echo "RETRY"
     if [ "$i" -lt $RETRY ]; then
       sleep 1
     fi
   fi
 done
 
+echo "FAIL"
 exit 1

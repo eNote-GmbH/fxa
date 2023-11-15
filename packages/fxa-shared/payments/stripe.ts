f77eaf84b76e513c8c6cf5204a5c46c75695680d
Reino Muhl <reino.muhl@gmail.com>
2023-11-15 12:31:22 -0500

    feat(auth,admin): add stripe key error on startup Because:
    * It is difficult to identify when a Stripe API key is no longer valid
    
    This commit:
    
    * Adds an error messsage to service logs on Stripe initialization
    
    Closes #FXA-8552

--- packages/fxa-shared/payments/stripe.ts
+++ packages/fxa-shared/payments/stripe.ts
@@ -76,6 +76,7 @@ export enum STRIPE_PRICE_ID_TO_IAP_ANALOG {
 }
 
 export type StripeHelperConfig = {
+  env: string;
   subscriptions: {
     stripeApiKey: string;
     stripeWebhookSecret: string;

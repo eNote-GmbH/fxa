9d1c90ae9a0beddaedf57fb59d370f73cc5bbce0
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2024-01-30 17:46:14 -0500

    fix(functional-tests): remove duplicate severity-2 describe groups
    Issue #FXA-8954

--- packages/functional-tests/tests/subscription-tests/subscription.spec.ts
+++ packages/functional-tests/tests/subscription-tests/subscription.spec.ts
@@ -98,142 +98,138 @@ test.describe('severity-2 #smoke', () => {
       test.slow();
     });
 
-    test.describe('severity-2', () => {
-      test('Metrics disabled: existing user checkout URL to not have flow params', async ({
-        pages: { settings, relier, page },
-      }, { project }) => {
-        test.skip(
-          project.name === 'production',
-          'test plan not yet available in prod'
-        );
-        // Go to settings page and verify the Data Collection toggle switch is visible
-        await settings.goto();
-        expect(await settings.dataCollection.isToggleSwitch()).toBe(true);
+    test('Metrics disabled: existing user checkout URL to not have flow params', async ({
+      pages: { settings, relier, page },
+    }, { project }) => {
+      test.skip(
+        project.name === 'production',
+        'test plan not yet available in prod'
+      );
+      // Go to settings page and verify the Data Collection toggle switch is visible
+      await settings.goto();
+      expect(await settings.dataCollection.isToggleSwitch()).toBe(true);
 
-        // Toggle swtich, verify the alert bar appears and its status
-        await settings.dataCollection.toggleShareData('off');
-        expect(await settings.alertBarText()).toContain('Opt out successful.');
-        expect(await settings.dataCollection.getToggleStatus()).toBe('false');
+      // Toggle swtich, verify the alert bar appears and its status
+      await settings.dataCollection.toggleShareData('off');
+      expect(await settings.alertBarText()).toContain('Opt out successful.');
+      expect(await settings.dataCollection.getToggleStatus()).toBe('false');
 
-        // Subscribe to plan and verify URL does not include flow parameter
-        await relier.goto();
-        await relier.clickSubscribe6Month();
-        expect(page.url()).not.toContain('&flow_begin_time=');
-      });
+      // Subscribe to plan and verify URL does not include flow parameter
+      await relier.goto();
+      await relier.clickSubscribe6Month();
+      expect(page.url()).not.toContain('&flow_begin_time=');
     });
 
-    test.describe('severity-2', () => {
-      test('Existing user checkout URL to have flow params', async ({
-        pages: { settings, relier, page },
-      }, { project }) => {
-        test.skip(
-          project.name === 'production',
-          'test plan not yet available in prod'
-        );
-        // Go to settings page and verify the Data Collection toggle switch is visible
-        await settings.goto();
-        expect(await settings.dataCollection.isToggleSwitch()).toBe(true);
+    test('Existing user checkout URL to have flow params', async ({
+      pages: { settings, relier, page },
+    }, { project }) => {
+      test.skip(
+        project.name === 'production',
+        'test plan not yet available in prod'
+      );
+      // Go to settings page and verify the Data Collection toggle switch is visible
+      await settings.goto();
+      expect(await settings.dataCollection.isToggleSwitch()).toBe(true);
 
-        // Toggle swtich and verify its status
-        await settings.dataCollection.toggleShareData('on');
-        expect(await settings.dataCollection.getToggleStatus()).toBe('true');
+      // Toggle swtich and verify its status
+      await settings.dataCollection.toggleShareData('on');
+      expect(await settings.dataCollection.getToggleStatus()).toBe('true');
 
-        // Subscribe to plan and verify URL does not include flow parameter
-        await relier.goto();
-        await relier.clickSubscribe6Month();
-        expect(page.url()).toContain('&flow_begin_time=');
+      // Subscribe to plan and verify URL does not include flow parameter
+      await relier.goto();
+      await relier.clickSubscribe6Month();
+      expect(page.url()).toContain('&flow_begin_time=');
+    });
+
+    test('New user checkout URL to have flow params', async ({
+      pages: { settings, relier, page },
+    }, { project }) => {
+      test.skip(
+        project.name === 'production',
+        'test plan not yet available in prod'
+      );
+      await settings.goto();
+      await settings.signOut();
+      await relier.goto();
+      await relier.clickSubscribe6Month();
+      expect(page.url()).toContain('&flow_begin_time=');
+    });
+
+    test('New user checkout URL to have flow params with cache cleared', async ({
+      pages: { settings, login, relier, page },
+    }, { project }) => {
+      test.skip(
+        project.name === 'production',
+        'test plan not yet available in prod'
+      );
+      await settings.goto();
+      await settings.signOut();
+      await login.clearCache();
+      await relier.goto();
+      await relier.clickSubscribe6Month();
+      expect(page.url()).toContain('&flow_begin_time=');
+    });
+
+    test('New user checkout URL to have RP-provided flow params, acquisition params & verify funnel metrics', async ({
+      pages: { settings, relier, page, subscribe },
+    }, { project }) => {
+      test.fixme(true, 'Fix required as of 2023/10/09 (see FXA-8447).');
+      test.skip(
+        project.name === 'production',
+        'test plan not yet available in prod'
+      );
+
+      let metricsObserver = new MetricsObserver(subscribe);
+      metricsObserver.startTracking();
+
+      await settings.goto();
+      await settings.signOut();
+      await relier.goto();
+
+      const subscribeUrl = await relier.getUrl();
+      if (!subscribeUrl) {
+        fail('Subscribe button has no href.');
+      }
+      const rpSearchParamsBefore = relier.getRpSearchParams(subscribeUrl);
+      const rpFlowParamsBefore = relier.getRpFlowParams(rpSearchParamsBefore);
+      const acquisitionParamsBefore =
+        relier.getRpAcquisitionParams(rpSearchParamsBefore);
+
+      // check flow metrics
+      await relier.clickSubscribeRPFlowMetrics();
+      const rpSearchParamsAfter = relier.getRpSearchParams(page.url());
+      const rpFlowParamsAfter = relier.getRpFlowParams(rpSearchParamsAfter);
+      expect(rpFlowParamsAfter).toStrictEqual(rpFlowParamsBefore);
+
+      // subscribe, failing first then succeeding
+      await subscribe.setEmailAndConfirmNewUser();
+      await subscribe.setConfirmPaymentCheckbox();
+      await subscribe.setFullName();
+      await subscribe.setFailedCreditCardInfo();
+      await subscribe.clickPayNow();
+      await subscribe.clickTryAgain();
+      await subscribe.setCreditCardInfo();
+      await subscribe.clickPayNow();
+      await subscribe.submit();
+
+      // check acquisition metrics
+      const acquisitionParamsAfter =
+        metricsObserver.getAcquisitionParamsFromEvents();
+      expect(acquisitionParamsAfter).toStrictEqual(acquisitionParamsBefore);
+
+      // check conversion funnel metrics
+      const expectedEventTypes = [
+        'amplitude.subPaySetup.view',
+        'amplitude.subPaySetup.engage',
+        'amplitude.subPaySetup.submit',
+        'amplitude.subPaySetup.fail',
+        'amplitude.subPaySetup.submit',
+        'amplitude.subPaySetup.success',
+      ];
+      const actualEventTypes = metricsObserver.rawEvents.map((event) => {
+        return event.type;
       });
-
-      test('New user checkout URL to have flow params', async ({
-        pages: { settings, relier, page },
-      }, { project }) => {
-        test.skip(
-          project.name === 'production',
-          'test plan not yet available in prod'
-        );
-        await settings.goto();
-        await settings.signOut();
-        await relier.goto();
-        await relier.clickSubscribe6Month();
-        expect(page.url()).toContain('&flow_begin_time=');
-      });
-
-      test('New user checkout URL to have flow params with cache cleared', async ({
-        pages: { settings, login, relier, page },
-      }, { project }) => {
-        test.skip(
-          project.name === 'production',
-          'test plan not yet available in prod'
-        );
-        await settings.goto();
-        await settings.signOut();
-        await login.clearCache();
-        await relier.goto();
-        await relier.clickSubscribe6Month();
-        expect(page.url()).toContain('&flow_begin_time=');
-      });
-
-      test('New user checkout URL to have RP-provided flow params, acquisition params & verify funnel metrics', async ({
-        pages: { settings, relier, page, subscribe },
-      }, { project }) => {
-        test.fixme(true, 'Fix required as of 2023/10/09 (see FXA-8447).');
-        test.skip(
-          project.name === 'production',
-          'test plan not yet available in prod'
-        );
-
-        let metricsObserver = new MetricsObserver(subscribe);
-        metricsObserver.startTracking();
-
-        await settings.goto();
-        await settings.signOut();
-        await relier.goto();
-
-        const subscribeUrl = await relier.getUrl();
-        if (!subscribeUrl) {
-          fail('Subscribe button has no href.');
-        }
-        const rpSearchParamsBefore = relier.getRpSearchParams(subscribeUrl);
-        const rpFlowParamsBefore = relier.getRpFlowParams(rpSearchParamsBefore);
-        const acquisitionParamsBefore =
-          relier.getRpAcquisitionParams(rpSearchParamsBefore);
-
-        // check flow metrics
-        await relier.clickSubscribeRPFlowMetrics();
-        const rpSearchParamsAfter = relier.getRpSearchParams(page.url());
-        const rpFlowParamsAfter = relier.getRpFlowParams(rpSearchParamsAfter);
-        expect(rpFlowParamsAfter).toStrictEqual(rpFlowParamsBefore);
-
-        // subscribe, failing first then succeeding
-        await subscribe.setEmailAndConfirmNewUser();
-        await subscribe.setConfirmPaymentCheckbox();
-        await subscribe.setFullName();
-        await subscribe.setFailedCreditCardInfo();
-        await subscribe.clickPayNow();
-        await subscribe.clickTryAgain();
-        await subscribe.setCreditCardInfo();
-        await subscribe.clickPayNow();
-        await subscribe.submit();
-
-        // check acquisition metrics
-        const acquisitionParamsAfter =
-          metricsObserver.getAcquisitionParamsFromEvents();
-        expect(acquisitionParamsAfter).toStrictEqual(acquisitionParamsBefore);
-
-        // check conversion funnel metrics
-        const expectedEventTypes = [
-          'amplitude.subPaySetup.view',
-          'amplitude.subPaySetup.engage',
-          'amplitude.subPaySetup.submit',
-          'amplitude.subPaySetup.fail',
-          'amplitude.subPaySetup.submit',
-          'amplitude.subPaySetup.success',
-        ];
-        const actualEventTypes = metricsObserver.rawEvents.map((event) => {
-          return event.type;
-        });
-        expect(actualEventTypes).toMatchObject(expectedEventTypes);
-      });
+      expect(actualEventTypes).toMatchObject(expectedEventTypes);
     });
   });
 });

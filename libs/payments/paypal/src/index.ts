8aeedf38846326276e8dea5c221c8141462f4ab5
Lisa Chan <lchan@mozilla.com>
2024-02-13 15:10:22 -0500

    feat(payments-paypal): Create PayPalManager

--- libs/payments/paypal/src/index.ts
+++ libs/payments/paypal/src/index.ts
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-export { PayPalClient } from './lib/paypal.client';
+export * from './lib/paypal.client';
 export { PayPalClientError, PayPalNVPError } from './lib/paypal.error';
+export * from './lib/paypal.manager';
 export * from './lib/types';
 export { nvpToObject, objectToNVP, isIpnMerchPmt } from './lib/util';

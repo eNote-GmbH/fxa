2d293a619654685ab490c497dba9b13095842edc
Reino Muhl <reino.muhl@gmail.com>
2024-03-12 11:20:51 -0400

    feat(stripe): add customerChanged method Because:
    * Move customerChanged method to shared library
    
    This commit:
    
    * Moves customerChanged to StripeManager
    
    Closes #FXA-8949

--- libs/payments/stripe/src/lib/stripe.manager.ts
+++ libs/payments/stripe/src/lib/stripe.manager.ts
@@ -5,10 +5,17 @@
 import { Injectable } from '@nestjs/common';
 import { Stripe } from 'stripe';
 import { StripeClient } from './stripe.client';
+import { AuthLogger, AuthRequest } from '@fxa/shared/types';
 
 @Injectable()
 export class StripeManager {
-  constructor(private client: StripeClient) {}
+  constructor(
+    private client: StripeClient,
+    private db: any,
+    private push: any,
+    private profile: any,
+    private log: AuthLogger
+  ) {}
 
   async isCustomerStripeTaxEligible(customer: Stripe.Customer) {
     if (!customer.tax) {
@@ -20,5 +27,17 @@ export class StripeManager {
       customer.tax?.automatic_tax === 'supported' ||
       customer.tax?.automatic_tax === 'not_collecting'
     );
+  }
+
+  async customerChanged(uid: string, email: string) {
+    const devices = await Promise.all([
+      this.db.devices(uid),
+      this.profile.deleteCache(uid),
+    ]);
+    await this.push.notifyProfileUpdated(uid, devices);
+    this.log.notifyAttachedServices('profileDataChange', {} as AuthRequest, {
+      uid,
+      email,
+    });
   }
 }
---
+++ libs/shared/types/src/lib/auth.types.ts
@@ -0,0 +1,76 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+import { Request, RequestApplicationState } from '@hapi/hapi';
+import { Token } from 'typedi';
+import { Logger } from 'mozlog';
+
+/**
+ * Auth-Server specific interfaces to use objects.
+ *
+ * These objects define valid interfaces for dynamically assembled objects
+ * used frequently in the fxa-auth-server.
+ *
+ */
+
+export interface AuthApp extends RequestApplicationState {
+  devices: Promise<any>;
+  locale: string;
+  acceptLanguage: string;
+  clientAddress: string;
+  metricsContext: any;
+  metricsEventUid?: string;
+  accountRecreated: boolean;
+  ua: {
+    browser: string;
+    browserVersion: string;
+    os: string;
+    osVersion: string;
+    deviceType: string;
+    formFactor: string;
+  };
+  isSuspiciousRequest: boolean;
+  isMetricsEnabled: Promise<boolean>;
+  geo: {
+    location?: {
+      city: string;
+      state: string;
+      country: string;
+      countryCode: string;
+      postalCode?: string;
+    };
+    [key: string]: any;
+  };
+}
+
+export interface AuthRequest extends Request {
+  auth: Request['auth'] & {
+    // AuthRequest will always have scopes present provided by
+    // the auth-oauth scheme
+    credentials: Request['auth']['credentials'] & { scope: string[] };
+  };
+  // eslint-disable-next-line no-use-before-define
+  log: AuthLogger;
+  app: AuthApp;
+  validateMetricsContext: any;
+  setMetricsFlowCompleteSignal: any;
+  emitMetricsEvent: any;
+  stashMetricsContext: any;
+  propagateMetricsContext: any;
+  gatherMetricsContext: any;
+}
+export interface AuthLogger extends Logger {
+  (tags: string | string[], data?: string | object): void;
+
+  begin(location: string, request: AuthRequest): void;
+
+  notifyAttachedServices(
+    serviceName: string,
+    request: AuthRequest,
+    data: Record<string, any>
+  ): Promise<void>;
+}
+
+// Container token types
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const AuthLogger = new Token<AuthLogger>('AUTH_LOGGER');

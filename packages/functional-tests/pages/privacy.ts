71ca26d28e8ae6527ed8fb52c0d64604bf6e3241
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2024-03-13 17:08:13 -0400

    refactor(functional-tests): legal tests Because:
    * we want to stabalize the functional test suite to improve it's success rate
    
    This commit:
    
    * renames tests
    * refactors the legal tests to use Page objects
    * uses fixme test annotations to skip tests requiring maintenance
    * removes uses of waitForTimeout and waitForSelector functions
    
    Relates to FXA-8960 & FXA-9280

---
+++ packages/functional-tests/pages/privacy.ts
@@ -0,0 +1,22 @@
+import { BaseLayout } from './layout';
+
+export const selectors = {};
+
+export class PrivacyPage extends BaseLayout {
+  readonly path = 'legal/privacy';
+
+  get backButton() {
+    return this.page.getByRole('button', { name: 'Back' });
+  }
+
+  get pageHeader() {
+    return this.page.getByRole('heading', {
+      name: 'Mozilla Accounts Privacy Notice',
+    });
+  }
+
+  async clickBackButton() {
+    await this.backButton.click();
+    await this.page.waitForURL(/\/legal$/);
+  }
+}

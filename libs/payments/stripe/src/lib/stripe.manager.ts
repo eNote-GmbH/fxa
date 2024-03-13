fca6b2d104f4e04d09202dc9f7cd0a02f4902814
Meghan Sardesai <msardesai@mozilla.com>
2024-03-12 23:31:38 -0400

    feat(payments-stripe): Create StripeManager addTaxIdToCustomer Because:
    * Part of M3a.
    * We want `addTaxIdToCustomer` added to StripeManager.
    
    This commit:
    
    * Adds aforementioned method to `StripeManager` using existing implementation for reference.
    * Updates customer metadata with `taxId` when customer `taxId` does not match incoming `taxId`.
    
    Closes FXA-8944

--- libs/payments/stripe/src/lib/stripe.manager.ts
+++ libs/payments/stripe/src/lib/stripe.manager.ts
@@ -21,4 +21,35 @@ export class StripeManager {
       customer.tax?.automatic_tax === 'not_collecting'
     );
   }
+
+  /**
+   * Adds the appropriate tax id
+   * if customer taxId does not match incoming taxId
+   **/
+  async addTaxIdToCustomer(customerId: string, taxId: string) {
+    // fetch customer by customerId
+    const customer = fetchCustomer(customerId, ['tax']);
+
+    // check that customer exists
+    if (customer) {
+      // check that customer does not already have taxId
+      if (!customer.tax && !customer.tax.includes(taxId)) {
+        // update customer with incoming taxId
+        const updatedCustomer = await this.stripe.customers.update(
+          customer.id,
+          {
+            invoice_settings: {
+              custom_fields: [{ name: MOZILLA_TAX_ID, value: taxId }],
+            },
+          }
+        );
+        return this.stripeFirestore.insertCustomerRecordWithBackfill(
+          customer.metadata.userid,
+          updatedCustomer
+        );
+      }
+    }
+
+    return;
+  }
 }

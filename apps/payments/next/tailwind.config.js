4d208da6fff438ab7e534ac43b3bed59097367f3
Lisa Chan <lchan@mozilla.com>
2024-03-15 00:52:50 -0400

    fix(shared-assets): Rename libs/shared/styles to libs/shared/assets using Nx CLI

--- apps/payments/next/tailwind.config.js
+++ apps/payments/next/tailwind.config.js
@@ -2,14 +2,12 @@ const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
 module.exports = {
-  presets: [
-    require('../../../libs/shared/styles/src/configs/tailwind.config')
-  ],
+  presets: [require('../../../libs/shared/assets/src/configs/tailwind.config')],
   content: [
     join(
       __dirname,
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
-  ]
-}
+  ],
+};
